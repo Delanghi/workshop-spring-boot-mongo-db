a3f9923e0b4cded6b0dca873806cd23d82041b62
Delanghi <daniel@langhi.com.br>
2019-03-28 11:25:08 +0100

    Primeiro commit - projeto criado

---
+++ src/main/java/com/dl/workshopmongo/resources/UserResource.java
@@ -0,0 +1,12 @@
+package com.dl.workshopmongo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController															// p/ informar que é um recurso REST
+@RequestMapping(value="/users")											// p/ o caminho "add ."
+public class UserResource {
+
+	
+	
+}
