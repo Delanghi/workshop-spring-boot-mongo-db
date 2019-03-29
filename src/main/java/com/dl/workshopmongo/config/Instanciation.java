efc5b7e553e36b89868dc687bed283070b5ca5ce
Delanghi <daniel@langhi.com.br>
2019-03-29 18:21:09 +0100

    Referenciando os posts do usuário

--- src/main/java/com/dl/workshopmongo/config/Instanciation.java
+++ src/main/java/com/dl/workshopmongo/config/Instanciation.java
@@ -43,5 +43,8 @@ public class Instanciation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));		
 				
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);											// p/ salvar Posts no usuário expecificado
+		
 	}
 }
